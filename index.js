3f9a4d78659c33d1ec9d1393828fd57c61f715ae
Muhammad Hasnain <hasnain0893@gmail.com>
2024-08-02 20:51:33 +0500

    how url work

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const http = require('http')
 const fs = require('fs')
-
+const url = require("url")
 
 const myServer = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') return res.end();
+    const log = `${Date.now()} : ${req.url} New Request Recived\n`
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl);
 
-    const log = `${Date.now()} : ${req.url}New Request Recived\n`
+
 
     fs.appendFile("log.txt", log, (err, data) => {
-        switch (req.url) {
+
+        switch (myUrl.pathname) {
             case '/': res.end("Hello From Home Page")
                 break;
-            case '/about': res.end("Hello From About")
+            case '/about':
+                const username = myUrl.query.myname
+                res.end(`Hi I Am ${username}`)
                 break;
             case '/contact': res.end("Hello From Contact us")
                 break;
