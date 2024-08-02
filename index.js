ddfe5585caa6fd50277aa8bcbca04ed79f1a4895
Muhammad Hasnain <hasnain0893@gmail.com>
2024-08-02 19:59:03 +0500

    working node js server

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const fs = require('fs')
 
 const myServer = http.createServer((req, res) => {
 
-    const log = `${Date.now()} : New Request Recived\n`
+    const log = `${Date.now()} : ${req.url}New Request Recived\n`
 
     fs.appendFile("log.txt", log, (err, data) => {
+        switch (req.url) {
+            case '/': res.end("Hello From Home Page")
+                break;
+            case '/about': res.end("Hello From About")
+                break;
+            case '/contact': res.end("Hello From Contact us")
+                break;
+            default: res.end('404 Not Found')
+        }
 
-        res.end("Hello From Server")
     })
 });
 myServer.listen(8000, () => { console.log("Server Started"); })
