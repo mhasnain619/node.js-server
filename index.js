81491b97df0a2a1c1ef2a699cfe9e6e2e148e0f3
Muhammad Hasnain <hasnain0893@gmail.com>
2024-08-02 23:59:17 +0500

    request Methods

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ const myServer = http.createServer((req, res) => {
     if (req.url === '/favicon.ico') return res.end();
     const log = `${Date.now()} : ${req.url} New Request Recived\n`
     const myUrl = url.parse(req.url, true)
-    console.log(myUrl);
 
 
 
     fs.appendFile("log.txt", log, (err, data) => {
 
         switch (myUrl.pathname) {
-            case '/': res.end("Hello From Home Page")
+            case '/':
+                if (req.method == 'GET') res.end("Hello From Home Page")
+
                 break;
             case '/about':
                 const username = myUrl.query.myname
@@ -21,6 +22,11 @@ const myServer = http.createServer((req, res) => {
                 break;
             case '/contact': res.end("Hello From Contact us")
                 break;
+            case "/signup":
+                if (req.method === 'GET') res.end('User Signup')
+                else if (req.method === 'POST') {
+                    res.end("Success")
+                }
             default: res.end('404 Not Found')
         }
 
